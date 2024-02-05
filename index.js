265363973e2bce98e292514b98cd76e5cd0685d4
devi0110 <devipopuri03@gmail.com>
2024-02-05 09:08:51 +0530

    version1

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+
+const app= express();
+const port=3000;
+const api_url=" https://v2.jokeapi.dev/joke/";
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/",(req,res)=>{
+    res.render("index.ejs")
+})
+
+app.post("/submit",async(req,res)=>{
+    try{
+    const result=await axios.get(api_url+"any?type=single");
+    console.log("used");
+    res.render("index.ejs", { content: result.data});
+    }
+    catch(error){
+        console.log(error.response.data);
+        res.status(404);
+
+
+    }
+});
+
+
+
+app.listen(port,()=>{
+    console.log(`hey working on port ${port}`);
+})
